ff07b55260478dd76afa34b2dc5abc902d8c10fa
Rohit Kumar <rkumar_be20@thapar.edu>
2022-09-06 01:21:26 +0530

    created default api doc

--- src/job/job.controller.ts
+++ src/job/job.controller.ts
@@ -2,31 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { JobService } from './job.service';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Job } from './entities/job.entity';
 
+@ApiTags('job')
 @Controller('job')
 export class JobController {
-  constructor(private readonly jobService: JobService) {}
+  constructor(private readonly jobService: JobService) { }
 
+  @ApiOkResponse({ description: 'Created new job.', type: Job })
   @Post()
   create(@Body() createJobDto: CreateJobDto) {
     return this.jobService.create(createJobDto);
   }
 
+  @ApiOkResponse({ description: 'Returned all jobs.', type: Job, isArray: true })
   @Get()
   findAll() {
     return this.jobService.findAll();
   }
 
+  @ApiOkResponse({ description: 'Returned job with ID.', type: Job })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.jobService.findOne(+id);
   }
 
+  @ApiOkResponse({ description: 'Updated job with ID.', type: Job })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
     return this.jobService.update(+id, updateJobDto);
   }
 
+  @ApiOkResponse({ description: 'Deleted job with ID.', type: Job })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.jobService.remove(+id);
--- src/utility/utility.controller.ts
+++ src/utility/utility.controller.ts
@@ -2,31 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UtilityService } from './utility.service';
 import { CreateUtilityDto } from './dto/create-utility.dto';
 import { UpdateUtilityDto } from './dto/update-utility.dto';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Utility } from './entities/utility.entity';
 
+@ApiTags('utility')
 @Controller('utility')
 export class UtilityController {
-  constructor(private readonly utilityService: UtilityService) {}
+  constructor(private readonly utilityService: UtilityService) { }
 
+  @ApiOkResponse({ description: 'Created new utility.', type: Utility })
   @Post()
   create(@Body() createUtilityDto: CreateUtilityDto) {
     return this.utilityService.create(createUtilityDto);
   }
 
+  @ApiOkResponse({ description: 'Returned all utilities.', type: Utility, isArray: true })
   @Get()
   findAll() {
     return this.utilityService.findAll();
   }
 
+  @ApiOkResponse({ description: 'Returned utility with ID.', type: Utility })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.utilityService.findOne(+id);
   }
 
+  @ApiOkResponse({ description: 'Updated utility with ID.', type: Utility })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUtilityDto: UpdateUtilityDto) {
     return this.utilityService.update(+id, updateUtilityDto);
   }
 
+  @ApiOkResponse({ description: 'Deleted utility with ID.', type: Utility })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.utilityService.remove(+id);
