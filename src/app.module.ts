9eee94b2710bdf310da7d0751976ff0049b6353f
Raghav Sharma <raghav_tinker@yahoo.com>
2022-09-07 20:15:58 +0530

    database and configuration setup done.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,23 @@ import { UserModule } from './user/user.module';
 import { JobModule } from './job/job.module';
 import { ChatModule } from './chat/chat.module';
 import { UtilityModule } from './utility/utility.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import configuration from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, JobModule, ChatModule, UtilityModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRoot(configuration().dbConfig.mongo_url),
+    AuthModule,
+    UserModule,
+    JobModule,
+    ChatModule,
+    UtilityModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
