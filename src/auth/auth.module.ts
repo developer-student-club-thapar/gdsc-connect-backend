68fe600a49ccd17135986d783783036f7124cba7
Rohit Kumar <rkumar_be20@thapar.edu>
2022-09-09 17:44:38 +0530

    [Fix BACK-9] Added User Authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,12 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import configuration from 'src/config/configuration';
 
 @Module({
   imports: [UserModule, PassportModule, JwtModule.register({
-    secret: jwtConstants.secret,
+    secret: configuration().jwtConfig.secret,
     signOptions: { expiresIn: '7d' },
   }),],
   controllers: [AuthController],
