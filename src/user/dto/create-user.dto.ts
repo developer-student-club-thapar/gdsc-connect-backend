9e8928baf186509b68dacfb9f68bdfedc1d9a250
Rohit Kumar <rkumar_be20@thapar.edu>
2022-09-10 04:25:22 +0530

    forgot to commit dto lol

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -8,13 +8,51 @@ export class NameDto {
   last: string;
 }
 
+export class SocialsDto {
+  @ApiProperty()
+  facebook: string;
+
+  @ApiProperty()
+  instagram: string;
+
+  @ApiProperty()
+  linkedin: string;
+
+  @ApiProperty()
+  twitter: string;
+
+  @ApiProperty()
+  github: string;
+};
+
 export class CreateUserDto {
+  @ApiProperty()
+  role: string;
+
   @ApiProperty()
   email: string;
 
   @ApiProperty()
-  password: string;
+  graduation_batch: number;
+
+  @ApiProperty()
+  tags: string[];
 
   @ApiProperty()
   name: NameDto;
+
+  @ApiProperty()
+  socials: SocialsDto;
+
+  @ApiProperty()
+  resume: string;
+
+  @ApiProperty()
+  bio: string;
+
+  @ApiProperty()
+  profile_picture: string;
+
+  @ApiProperty()
+  password: string;
 }
