46e3acdc4e47e245d42b909a225a59bddae732cd
Rohit Kumar <rkumar_be20@thapar.edu>
2022-09-10 02:24:57 +0530

    changed user schema, added temp /users:email/tags

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
 
+export class NameDto {
+  @ApiProperty()
+  first: string;
+
+  @ApiProperty()
+  last: string;
+}
+
 export class CreateUserDto {
   @ApiProperty()
   email: string;
 
   @ApiProperty()
   password: string;
+
+  @ApiProperty()
+  name: NameDto;
 }
--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,13 +1,81 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 
+export class Name {
+  @ApiProperty()
+  first: string;
+
+  @ApiProperty()
+  last: string;
+}
+
+export class Socials {
+  @ApiProperty()
+  facebook: string;
+
+  @ApiProperty()
+  instagram: string;
+
+  @ApiProperty()
+  linkedin: string;
+
+  @ApiProperty()
+  twitter: string;
+
+  @ApiProperty()
+  github: string;
+};
+
 @Schema()
 export class User {
-  @Prop()
+
+  @Prop({
+    enum: ['admin', 'member'],
+    default: 'member',
+    required: true
+  })
+  @ApiProperty()
+  role: string;
+
+  @Prop({ required: true, unique: true })
   @ApiProperty()
   email: string;
 
-  @Prop({ select: false })
+  @Prop({ required: true })
+  @ApiProperty()
+  graduation_batch: number;
+
+  @Prop({
+    type: [{
+      type: String,
+      enum: ['tag1', 'tag2', 'tag3'],
+      required: false
+    }], default: []
+  })
+  @ApiProperty()
+  tags: string[];
+
+  @Prop({ type: Name, required: true })
+  @ApiProperty()
+  name: Name;
+
+  @Prop({ type: Socials, required: false })
+  @ApiProperty()
+  socials: Socials;
+
+  @Prop({ required: false })
+  @ApiProperty()
+  resume: string;
+
+  @Prop({ required: false })
+  @ApiProperty()
+  bio: string;
+
+  @Prop({ required: false })
+  @ApiProperty()
+  profile_picture: string;
+
+  @Prop({ select: false, required: true })
   @ApiProperty()
   password: string;
 }
