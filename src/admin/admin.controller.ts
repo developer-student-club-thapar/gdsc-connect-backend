78c6b628c897f668f4f2eac350dc63ab9bbc41d8
Raghav Sharma <71115825+raghavTinker@users.noreply.github.com>
2022-10-10 09:49:42 +0530

    using admin guard

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,12 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ResourceDecorator } from 'src/resource.decorator';
 import { AdminService } from './admin.service';
+import { AdminGuard } from './guards/admin.guard';
 
 @ApiTags('admin')
 @ApiBearerAuth()
 @ResourceDecorator('admin')
 @Controller('admin')
+@UseGuards(AdminGuard)
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 }
